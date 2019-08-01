59adea3ac1c47161761c7ffe431296d5aba7c7d8
fedot79 <39196719+fedot79@users.noreply.github.com>
2019-08-01 17:39:05 +0300

    1.08
    Доделан вывод категорий в сайдбар

---
+++ tpl/sidebar.php
@@ -0,0 +1,29 @@
+<?php require_once 'models/functions.php';
+$categories = getCategories($link);
+
+
+
+?>
+<div class="col-md-2 left-menu">
+    <p class="left-menu-cat">Categories</p>
+    <nav >
+
+        <li id="all" class="act menu-head" >ALL</li>
+            <?php
+            foreach ($categories as $category) {
+               if ($category["parent_id"] == 0){
+                   echo ('<li class="all-child menu-head fict ">'.$category["name"].'</li>');
+                   $child = $category["id"];
+
+               }if($category["parent_id"] == $child){
+                   echo ('<li class="fict-child"><a href="category_page.php">'.$category["name"].'</a></li>');
+               }
+
+
+
+            };
+            ?>
+
+
+    </nav>
+</div>
