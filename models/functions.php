b358b4d3ba3000a85553ec94f6a8ac33e8471e49
fedot79 <39196719+fedot79@users.noreply.github.com>
2019-08-13 14:28:11 +0300

    13.08
    Сделан вывод страницы товара из БД

--- models/functions.php
+++ models/functions.php
@@ -23,3 +23,9 @@ function booksCategories($link,$cat_id){
     $bookCat = mysqli_query($link,$query);
     return $bookCat;
 }
+function getBook($link,$book_id){
+    $query = "SELECT * FROM books WHERE id = '$book_id'";
+    $res = mysqli_query($link,$query);
+    $book = mysqli_fetch_all($res,MYSQLI_ASSOC);
+    return $book;
+}
